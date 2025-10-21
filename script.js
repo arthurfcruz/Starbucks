ddc5570daa8466f248a1223237e42916390f8cfd
Arthur Felix Cruz <arthurfelixcruz@gmail.com>
2025-10-21 01:45:34 -0300

    adicionando a funcao changeImage

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let circle = document.querySelector(".circle")
+let cup = document.querySelector(".cup")
 
 function changeColor(color){
     circle.style.backgroundColor = color
+}
+
+function changeImage(image){
+    cup.src = image
 }
